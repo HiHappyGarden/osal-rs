e0818f03ce0face31c12fced4101d46585f06632
Antonio Salsi <passy.linux@zresa.it>
2026-07-30 20:53:20 +0200

    refactor: rename get_current_time_us to get_current_time for consistency

--- osal-rs-tests/src/freertos/api_surface.rs
+++ osal-rs-tests/src/freertos/api_surface.rs
@@ -104,7 +104,7 @@ pub fn compile_system_surface() {
     let _resume_all: fn() -> types::BaseType = <System as SystemFn>::resume_all;
     let _stop: fn() = <System as SystemFn>::stop;
     let _get_tick_count: fn() -> types::TickType = <System as SystemFn>::get_tick_count;
-    let _get_current_time_us: fn() -> Duration = <System as SystemFn>::get_current_time_us;
+    let _get_current_time: fn() -> Duration = <System as SystemFn>::get_current_time;
     let _get_ms_from_tick: fn(&Duration) -> types::TickType = <System as SystemFn>::get_ms_from_tick;
     let _count_threads: fn() -> usize = <System as SystemFn>::count_threads;
     let _get_all_thread: fn() -> SystemState = <System as SystemFn>::get_all_thread;
